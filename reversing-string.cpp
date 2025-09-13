b561877f532ef288c1f87d297333994634be1160
Sameer Yasin <113780206+SameerYasin@users.noreply.github.com>
2025-09-13 09:32:11 +0530

    Create reversing-string.cpp

---
+++ reversing-string.cpp
@@ -0,0 +1,54 @@
+// Name    : Sameer Yasin
+// PRN     : 24070123092
+// Division: B1
+// Experiment: 10-D
+
+#include <iostream>
+#include <cstdio>
+#include <string>
+
+using namespace std;
+
+void swap_chars(char& a, char& b) {
+    char temp = a;
+    a = b;
+    b = temp;
+}
+
+void ReversingString(string& str) {
+    int n = str.length();
+    for (int i = 0; i < n / 2; i++) {
+        swap_chars(str[i], str[n - i - 1]);
+    }
+}
+
+int main() {
+    string myString;
+    
+    cout << "Enter a string to reverse: ";
+    getline(cin, myString);
+
+    cout << "Original string: " << myString << endl;
+
+    ReversingString(myString);
+
+    cout << "Reversed string: " << myString << endl;
+    return 0;
+}
+
+/*
+OUTPUT :
+Enter a string to reverse: Madam
+Original string: Madam
+Reversed string: madaM
+
+
+=== Code Execution Successful ===
+
+Enter a string to reverse: Hello world!
+Original string: Hello world!
+Reversed string: !dlrow olleH
+
+
+=== Code Execution Successful ===
+*/
